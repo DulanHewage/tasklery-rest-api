5090ba7ca27ba9ab849235278581081faa120a84
Dulan Hewage <dulanhewage2@gmail.com>
2024-02-04 01:10:00 +0530

    Update user model, implement save password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,70 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 interface IUser {
-  name: string;
   email: string;
-  // define other fields here
+  firstName: string;
+  lastName: string;
+  password: string;
+  verified?: boolean;
+  verificationToken?: string;
 }
 
 interface IUserDocument extends IUser, Document {
   _id: Schema.Types.ObjectId;
 }
 
-const userSchema = new mongoose.Schema<IUserDocument>({
-  name: String,
-  email: String,
-  // define other fields here
+const userSchema = new mongoose.Schema<IUserDocument>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: 6,
+    },
+    firstName: { type: String, required: true, trim: true, minlength: 2 },
+    lastName: { type: String, required: true, trim: true, minlength: 2 },
+    password: { type: String, required: true, trim: true, minlength: 8 },
+    verified: { type: Boolean, default: false },
+    verificationToken: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      default: () => crypto.randomBytes(20).toString("hex"),
+    },
+    // define other fields here
+  },
+  { timestamps: true }
+);
+
+// helper methods
+async function hashPassword(password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+}
+
+// pre-save hook
+userSchema.pre<IUserDocument>("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      this.password = await hashPassword(this.password);
+      return next();
+    } catch (error) {
+      return next(error as Error);
+    }
+  }
+  return next();
 });
+
+userSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword: string
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User: Model<IUserDocument> = mongoose.model("User", userSchema);
 
